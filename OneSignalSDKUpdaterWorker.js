170dc71749c307ac0fa92b0514562a0b1ba28ca1
Alan3r <83416730+Alan3r@users.noreply.github.com>
2025-06-19 12:35:50 +0200

    Updates

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,1 @@
-// This file is required by OneSignal for push notifications. See https://documentation.onesignal.com/docs/web-push-setup
-// You can leave this file empty or customize it if needed.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
