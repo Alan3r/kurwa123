86b1bce6b414df5cd7d86c9a685c9dd738a8aec0
Alan3r <83416730+Alan3r@users.noreply.github.com>
2025-06-19 12:38:19 +0200

    Updates

---
+++ OneSignalSDK.sw.js
@@ -0,0 +1,5 @@
+/*
+ OneSignalSDK.sw.js - Service Worker for OneSignal Web Push
+ See: https://github.com/OneSignal/OneSignal-Website-SDK/blob/main/onesignal-public-files/OneSignalSDK.sw.js
+*/
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
